fd6346c01e3423be7bb8c69503a58338ce2498cc
Your Name <you@example.com>
2020-11-17 00:08:10 -0300

    first commit

---
+++ Curso para desenvolvimento de jobs com Spring Batch/PrimeiroProjeto/src/main/java/com/github/primeiroprojetospringbatch/PrimeiroprojetospringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.github.primeiroprojetospringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroprojetospringbatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeiroprojetospringbatchApplication.class, args);
+    }
+
+}
